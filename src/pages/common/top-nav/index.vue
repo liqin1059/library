42147ff839be2b47cf754eb5c52e3eaba56473b6
liqin <liqin.xd@haiermoney.com>
2018-12-20 20:22:46 +0800

    title

--- src/pages/common/top-nav/index.vue
+++ src/pages/common/top-nav/index.vue
@@ -1,5 +1,6 @@
 <template>
   <header class="nav-top-header">
+    <h1>书籍管理系统</h1>
     <div class="user-name">用户名</div>
   </header>
 </template>
@@ -20,6 +21,15 @@ export default {
     float: right;
     margin-right: 40px;
     cursor: pointer;
+  }
+  h1{
+    margin: 0;
+    display: inline-block;
+    font-size: 20px;
+    font-weight: 600;
+    width: 160px;
+    padding-left: 40px;
+    background: #1674d5;
   }
 }
 </style>
